5bbd799b12e4f43c22bec66eb76a581e79595575
JarbasAi <jarbasai@mailfence.com>
2024-03-15 03:53:18 +0000

    automodernize (untested)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,10 @@
 from os.path import isfile
 
-from mycroft import MycroftSkill, intent_file_handler
-from mycroft.util import play_wav
+from ovos_workshop.skills import OVOSSkill
+from ovos_workshop.decorators import intent_handler
 
 
-class AnimalSounds(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+class AnimalSounds(OVOSSkill):
 
     def sound_file_path(self, animal):
         if animal[-1] == 's':
@@ -15,18 +13,11 @@ class AnimalSounds(MycroftSkill):
         self.log.info(path)
         return path if isfile(path) else None
 
-    @intent_file_handler('sounds.animal.intent')
+    @intent_handler('sounds.animal.intent')
     def handle_sounds_animal(self, message):
         animal = message.data.get('animal')
         sound_file = self.sound_file_path(animal)
         if sound_file:
-            play_wav(sound_file)
+            self.play_audio(sound_file)
         else:
-            self.speak_dialog('not.sure', data={
-                'animal': animal
-            })
-
-
-def create_skill():
-    return AnimalSounds()
-
+            self.speak_dialog('not.sure', data={'animal': animal})
